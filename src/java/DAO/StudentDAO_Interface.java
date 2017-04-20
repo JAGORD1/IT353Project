17a5c2d75f4de49c7eb314091b05f76561cdc778
burningcode7 <mjdifig@ilstu.edu>
2017-04-19 20:43:23 -0500

    Add files via upload

--- src/java/DAO/StudentDAO_Interface.java
+++ src/java/DAO/StudentDAO_Interface.java
@@ -12,7 +12,7 @@ public interface StudentDAO_Interface {
     public int createStudent(StudentBean studentDAO);
     public boolean checkStudentEmail(String email);      
     public boolean studentLogin(String email, String password);
-    public ArrayList<StudentBean> findStudents(ArrayList<String> searchInfo);
+    public ArrayList<StudentBean> findStudents(StudentBean searchStudent); //first name, last name, act, sat, psat, major, university
     public int updateStudent(StudentBean studentDAO, String originalEmail);
     public StudentBean getStudentInfo(String email);
     public int changePassword(String email, String password);
