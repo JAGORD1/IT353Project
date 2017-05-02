868494685313f99cee8b19d93b448a57be24a9ac
Cody Ferguson <c0d12395@gmail.com>
2017-05-02 16:21:38 -0500

    Add files via upload

--- src/java/Controller/CalendarView.java
+++ src/java/Controller/CalendarView.java
@@ -15,8 +15,8 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
  
-import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
+//import org.primefaces.context.RequestContext;
+//import org.primefaces.event.SelectEvent;
  
 @ManagedBean
 public class CalendarView {
@@ -33,17 +33,17 @@ public class CalendarView {
     private Date date10;
     private Date date11;
      
-    public void onDateSelect(SelectEvent event) {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
-    }
+//    public void onDateSelect(SelectEvent event) {
+//        FacesContext facesContext = FacesContext.getCurrentInstance();
+//        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+//    }
      
     public void click() {
-        RequestContext requestContext = RequestContext.getCurrentInstance();
+        //RequestContext requestContext = RequestContext.getCurrentInstance();
          
-        requestContext.update("form:display");
-        requestContext.execute("PF('dlg').show()");
+        //requestContext.update("form:display");
+        //requestContext.execute("PF('dlg').show()");
     }
  
     public Date getDate1() {
